68770b6770db2821955157a1b0703487f6bd86cf
Emmanuel De Freitas <manny.defreitas7@me.com>
2026-06-26 00:30:08 -0700

    update demo-app

--- demo/app/src/bun/index.ts
+++ demo/app/src/bun/index.ts
@@ -1,8 +1,19 @@
-import { python } from "bun_python";
-import { BrowserWindow, Updater } from "electrobun/bun";
+
+import { BrowserWindow, Updater, Screen } from "electrobun/bun";
+import { retrieve, store } from "./storage";
+import { getAuth, end } from "./example-usage";
 
 const DEV_SERVER_PORT = 5173;
 const DEV_SERVER_URL = `http://localhost:${DEV_SERVER_PORT}`;
+
+const primary = Screen.getPrimaryDisplay();
+const windowWidth = 800;
+const windowHeight = 600;
+
+// Calculate centered position
+const x = Math.round((primary.workArea.width - windowWidth) / 2) + primary.workArea.x;
+const y = Math.round((primary.workArea.height - windowHeight) / 2) + primary.workArea.y;
+
 
 // Check if Vite dev server is running for HMR
 async function getMainViewUrl(): Promise<string> {
@@ -27,14 +38,51 @@ const url = await getMainViewUrl();
 const mainWindow = new BrowserWindow({
 	title: "CorporaPy testing",
   url,
-  titleBarStyle: 'hiddenInset',
+  hidden: true,
+  titleBarStyle: "hiddenInset",
+  renderer: 'native',
   passthrough: true,
-	frame: {
-		width: 900,
-		height: 700,
-		x: 200,
-		y: 200,
-	},
+  frame: {
+    width: windowWidth,
+       height: windowHeight,
+       x,
+       y
+  },
+  styleMask: {
+      // These are the current defaults
+      Borderless: false,
+      Titled: true,
+      Closable: true,
+      Miniaturizable: false,
+      Resizable: false,
+      UnifiedTitleAndToolbar: true,
+      FullScreen: false,
+      FullSizeContentView: false,
+      UtilityWindow: false,
+      DocModalWindow: false,
+      NonactivatingPanel: true,
+      HUDWindow: false,
+    }
 });
 
-console.log("React Tailwind Vite app started!");
+const start = async () => {
+  try {
+    const state = await retrieve();
+    if (!state) {
+      // This is probably the first time the app is being run, so no state yet.
+      // We will create a new window and store it as the active window.
+      mainWindow.show();
+      store({ activeWindow: mainWindow, windows: [mainWindow.id], theme: "dark" });
+      return;
+    };
+    if (state.activeWindow.id != mainWindow.id) {
+      mainWindow.show();
+    }
+
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+await start();
+await getAuth();
--- demo/app/src/mainview/App.tsx
+++ demo/app/src/mainview/App.tsx
@@ -1,14 +1,15 @@
-import { Surface } from '@heroui/react';
-import { Typography } from "@heroui/react";
+import { Surface, Typography, Button } from '@heroui/react';
 import {Icon} from "@iconify/react";
-import { Button } from "@heroui/react";
 
 function App() {
 
 	return (
-	<main className="flex min-h-screen items-center justify-center">
-      <Surface>
-        <Typography>Login</Typography>
+	<main className="flex min-h-screen items-center justify-center select-none">
+      <Surface className='p-6 border-2 border-neutral-100 rounded-2xl w-1/3' variant="default">
+
+
+        <Typography type="h2">Login</Typography>
+        <div className="flex flex-col gap-3 mt-4">
         <Button className="w-full" variant="tertiary">
               <Icon icon="devicon:google" />
               Sign in with Google
@@ -17,8 +18,8 @@ function App() {
 
               <Icon icon="ion:logo-apple" />
               Sign in with Apple
-            </Button>
-
+          </Button>
+        </div>
       </Surface>
 			</main>
 	);
