5c4461154070ab1f1278c7335f95a548e5ba1cc1
Emmanuel De Freitas <manny.defreitas7@me.com>
2026-06-28 08:32:13 -0700

    add dependencies.

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), reactRouter()],
   root: "src/mainview",
   build: {
-    outDir: "../../dist",
+    outDir: "dist",
     emptyOutDir: true,
   },
   server: {
